d44609a9034c59307e0aa2d486a9405dee360640
MangaD <davidmanuel1993@hotmail.com>
2019-03-25 20:34:30 +0000

    static fields

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 
 public class Main {
 
+	public static int port = 4444;
 	public static Database db = null;
-	
+	public static String db_name = "p2photo.db";
+
 	public static void main(String[] args) {
 		
 		try {
-			db = new Database("p2photo.db");
+			db = new Database(db_name);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 		db.selectAllUsers();
 
-		ClientListener service = new ClientListener(4444, "ServiceThread");
+		ClientListener service = new ClientListener(port, "ServiceThread");
 		service.start();
 	}
 
