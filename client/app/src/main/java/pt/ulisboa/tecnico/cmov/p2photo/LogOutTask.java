430242c005fa186c917ec79106fa3adf8e0f42a6
MangaD <davidmanuel1993@hotmail.com>
2019-04-22 18:02:40 +0100

    improve some code

---
+++ client/app/src/main/java/pt/ulisboa/tecnico/cmov/p2photo/LogOutTask.java
@@ -0,0 +1,52 @@
+package pt.ulisboa.tecnico.cmov.p2photo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+
+public class LogOutTask extends AsyncTask<Void, Void, Void> {
+
+    private WeakReference<LoggedInActivity> activityReference;
+    private ProgressDialog pd;
+    GlobalClass context;
+
+    public LogOutTask(GlobalClass ctx, LoggedInActivity activity) {
+
+        activityReference = new WeakReference<>(activity);
+        this.context = ctx;
+        // Create Progress dialog
+        pd = new ProgressDialog(activity);
+        pd.setMessage(ctx.getString(R.string.logging_out));
+        pd.setTitle("");
+        pd.setIndeterminate(true);
+        pd.setCancelable(false);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd.show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... values) {
+        ServerConnection conn = context.getConnection();
+
+        conn.disconnect();
+        Log.d("LoggedInActivity", "Disconnected");
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void v) {
+        pd.dismiss();
+        Log.d("LoggedInActivity", context.getString(R.string.log_out));
+        Toast.makeText(activityReference.get().getApplicationContext(), context.getString(R.string.log_out), Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(activityReference.get(), LoginActivity.class);
+        activityReference.get().startActivity(intent);
+    }
+}
--- client/app/src/main/java/pt/ulisboa/tecnico/cmov/p2photo/LoggedInActivity.java
+++ client/app/src/main/java/pt/ulisboa/tecnico/cmov/p2photo/LoggedInActivity.java
@@ -1,24 +1,15 @@
 package pt.ulisboa.tecnico.cmov.p2photo;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.drive.DriveClient;
 import com.google.android.gms.drive.DriveResourceClient;
 
-import java.lang.ref.WeakReference;
-
 public class LoggedInActivity extends AppCompatActivity {
-
-    private static final String TAG = "loggedin";
 
     private DriveClient mDriveClient;
     private DriveResourceClient mDriveResourceClient;
@@ -42,76 +33,56 @@ public class LoggedInActivity extends AppCompatActivity {
          * CREATE ALBUM
          */
         Button buttonCreateAlbum = findViewById(R.id.button_create_album);
-        buttonCreateAlbum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoggedInActivity.this, CreateAlbumActivity.class);
-                startActivity(intent);
-            }
+        buttonCreateAlbum.setOnClickListener((View view) -> {
+            Intent intent = new Intent(LoggedInActivity.this, CreateAlbumActivity.class);
+            startActivity(intent);
         });
 
         /**
          * ADD PHOTO TO ALBUM
          */
         Button buttonAddPhoto = findViewById(R.id.button_add_photo);
-        buttonAddPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        buttonAddPhoto.setOnClickListener((View view) -> {
+            setDriveVars();
 
-                setDriveVars();
-
-                Intent intent = new Intent(LoggedInActivity.this, AddPhotoActivity.class);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(LoggedInActivity.this, AddPhotoActivity.class);
+            startActivity(intent);
         });
         /**
          * ADD USER TO ALBUM
          */
         Button buttonAddUserToAlbum = findViewById(R.id.button_add_user_to_album);
-        buttonAddUserToAlbum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoggedInActivity.this, AddUserToAlbumActivity.class);
-                startActivity(intent);
-            }
+        buttonAddUserToAlbum.setOnClickListener((View view) -> {
+            Intent intent = new Intent(LoggedInActivity.this, AddUserToAlbumActivity.class);
+            startActivity(intent);
         });
         /**
          * LIST USER'S ALBUMS
          */
         Button buttonListAlbums = findViewById(R.id.button_list_user_album);
-        buttonListAlbums.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        buttonListAlbums.setOnClickListener((View view) -> {
+            setDriveVars();
 
-                setDriveVars();
-
-                //Intent intent = new Intent(LoggedInActivity.this, ListUserAlbumActivity.class);
-                Intent intent = new Intent(LoggedInActivity.this, ListUserAlbumActivity2.class);
-                startActivity(intent);
-            }
+            //Intent intent = new Intent(LoggedInActivity.this, ListUserAlbumActivity.class);
+            Intent intent = new Intent(LoggedInActivity.this, ListUserAlbumActivity2.class);
+            startActivity(intent);
         });
 
         /**
          * LOG OUT
          */
         Button buttonLogOut = findViewById(R.id.button_log_out);
-        buttonLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new LogOutTask(LoggedInActivity.this).execute();
-            }
-        });
+        buttonLogOut.setOnClickListener((View view) ->
+            new LogOutTask((GlobalClass) getApplicationContext(), LoggedInActivity.this).execute()
+        );
 
         /*
          * View Photo Test
          * */
         Button buttonPhotoTest = findViewById(R.id.button_photo_test);
-        buttonPhotoTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoggedInActivity.this, ViewPhotoActivity.class);
-                startActivity(intent);
-            }
+        buttonPhotoTest.setOnClickListener((View view) -> {
+            Intent intent = new Intent(LoggedInActivity.this, ViewPhotoActivity.class);
+            startActivity(intent);
         });
 
     }
@@ -124,68 +95,4 @@ public class LoggedInActivity extends AppCompatActivity {
         globalVariable.setmDriveResourceClient(mDriveResourceClient);
     }
 
-    /**
-     * Shows a toast message.
-     */
-    protected void showMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
-
-
-    protected DriveClient getDriveClient() {
-        return mDriveClient;
-    }
-
-    protected DriveResourceClient getDriveResourceClient() {
-        return mDriveResourceClient;
-    }
-
-
-    /**
-     * Server side tasks.
-     */
-
-    private static class LogOutTask extends AsyncTask<Void, Void, Void> {
-
-        private WeakReference<LoggedInActivity> activityReference;
-        private ProgressDialog pd;
-        Context ctx;
-
-        private LogOutTask(LoggedInActivity activity) {
-
-            activityReference = new WeakReference<>(activity);
-            ctx = activity.getApplicationContext();
-            // Create Progress dialog
-            pd = new ProgressDialog(activity);
-            pd.setMessage(ctx.getString(R.string.logging_out));
-            pd.setTitle("");
-            pd.setIndeterminate(true);
-            pd.setCancelable(false);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pd.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... values) {
-            GlobalClass context = (GlobalClass) activityReference.get().getApplicationContext();
-            ServerConnection conn = context.getConnection();
-
-            conn.disconnect();
-            Log.d("LoggedInActivity", "Disconnected");
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void v) {
-            pd.dismiss();
-            Log.d("LoggedInActivity", ctx.getString(R.string.log_out));
-            Toast.makeText(activityReference.get().getApplicationContext(), ctx.getString(R.string.log_out), Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(activityReference.get(), LoginActivity.class);
-            activityReference.get().startActivity(intent);
-        }
-    }
 }
